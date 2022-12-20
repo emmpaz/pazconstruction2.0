de5e3c0397223ee63b9c1e10be6eeb6349b96548
epaz <epaz@iastate.edu>
2022-12-20 13:21:56 -0600

    header added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+import { Component } from 'react';
+import './css/App.css';
+import blueHeader from './pics/blue-overlay.png';
+import Header from './Header';
+
+class App extends Component{
+  render(){
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="blueHeader">
+        <Header/>
+        <img className="img-header" src={blueHeader}/>
+      </div>
     </div>
-  );
+  )
+}
 }
 
 export default App;
