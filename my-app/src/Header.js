201ed2dcf9f5f8e342cfa455a0b61cc9add1b58c
epaz <epaz@iastate.edu>
2022-12-28 21:57:10 -0600

    menu

--- my-app/src/Header.js
+++ my-app/src/Header.js
@@ -5,9 +5,17 @@ import logo from './pics/logo.png';
 class Header extends Component{
 
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            menuBar : false
+        }
     }
 
+    menuBarToggle = () => {
+        this.setState(prevSate => ({
+            menuBar : !prevSate.menuBar
+        }))
+    }
 
     render(){
         return(
@@ -22,6 +30,17 @@ class Header extends Component{
                 <div className="LocationHeaderContainer">
                     <p style={{cursor: 'pointer'}} onClick={this.props.locationClick}>location</p>
                     <p style={{cursor: 'pointer'}} onClick={this.props.aboutUsClick}>about us</p>
+                </div>
+                <div className="container" onClick={this.menuBarToggle}>
+                    <div className={this.state.menuBar ? "change bar1":"bar1"}></div>
+                    <div className={this.state.menuBar ? "change bar2":"bar2"}></div>
+                    <div className={this.state.menuBar ? "change bar3":"bar3"}></div>
+                </div>
+                <div className="sideBar">
+                    <p>Gallery</p>
+                    <p>Location</p>
+                    <p>About Us</p>
+                    <p>Contact</p>
                 </div>
             </div>
         )
